922f38c76acb656a2a2d2135fbd812b020a7a4d6
Codins <shaivilisthebest@gmail.com>
2025-04-27 23:14:36 -0500

    feat: Started work on making login page look better

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require 'db.php';
-
+require 'header.php';
 $error = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -26,15 +26,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html>
-<head><title>Login</title></head>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/login.css">
+</head>
 <body>
-    <h1>Login</h1>
-    <form method="post">
-        Email: <input name="email" required><br>
-        Password: <input type="password" name="password" required><br>
-        <button type="submit">Login</button>
-    </form>
-    <p style="color:red;"><?= htmlspecialchars($error) ?></p>
+    <div class="login-container">
+        <h1>Login</h1>
+        <form method="post" class="login-form">
+            <label for="email">Email:</label>
+            <input id="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input id="password" type="password" name="password" required>
+
+            <button type="submit">Login</button>
+        </form>
+        <?php if ($error): ?>
+            <p class="login-alert"><?= htmlspecialchars($error) ?></p>
+        <?php endif; ?>
+    </div>
 </body>
 </html>
-
